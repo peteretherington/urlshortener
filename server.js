f916ed1db2a29ccc9b6bb19de427b4c87cee01c3
Peter Etherington <pjg.etherington@gmail.com>
2020-01-09 11:25:44 -0500

    db connected and saving items to url collection

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 'use strict';
-require('dotenv').config();
-const express = require('express');
-// const mongo = require('mongodb');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
 
+// Get environment variables
+require('dotenv').config();
+
+const cors = require('cors');
+const dns = require('dns');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const mongo = require('mongodb');
 const app = express();
 
-mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-
-const Schema = mongoose.Schema;
+// Connect to database
+mongoose.connect(
+	process.env.MONGOLAB_URI,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	},
+	() => console.log(mongoose.connection.readyState)
+);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
-// you should mount the body-parser here
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -28,27 +32,38 @@ app.get('/', function(req, res) {
 	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-const urlSchema = new Schema({
-	url:  String,
-	shortURL: String,
+const URLSchema = new mongoose.Schema({
+	original_url: String,
+	short_url: String
 });
 
-const Url = mongoose.model('url', urlSchema);
+const URL = mongoose.model('URL', URLSchema);
 
-const shortenURL = (req, res) => {
-	const createAndSaveURL = (done) => {
-		const newUrl = new Url({
-			originalURL: req.body.url,
-			shortenedURL: 1,
-		});
-	
-		newUrl.save((err, data) => {
-			if (err) return console.error(err);
-			done(null, data);
-		});
-	};
+const createAndSaveURL = (url, done) => {
+	const item = new URL({
+		original_url: url,
+		short_url: String(Math.floor(Math.random() * 100))
+	});
 
-	res.json({ url: req.body.url });
+	item.save((err, data) => (err ? console.error(err) : done(null, data)));
+};
+
+const shortenURL = (req, res, done) => {
+	const httpRegex = /http[s]*:\/\/[www.]*/;
+	const { url } = req.body;
+	const dnsURL = url.replace(httpRegex, '');
+
+	// Test if URL is valid
+	dns.lookup(dnsURL, err => {
+		if (err) return res.json({ error: 'Invalid URL' });
+
+		createAndSaveURL(url, done);
+
+		res.json({
+			original_url: url,
+			short_url: 'unkown'
+		});
+	});
 };
 
 // Shorten URL API endpoint
@@ -59,8 +74,6 @@ app.listen(process.env.PORT, function() {
 });
 
 /*
-dns.lookup(host, cb)
-
 1. I can POST a URL to [project_url]/api/shorturl/new and I will receive a shortened URL in the JSON response. Example : {"original_url":"www.google.com","short_url":1}
 
 2. If I pass an invalid URL that doesn't follow the http(s)://www.example.com(/more/routes) format, the JSON response will contain an error like {"error":"invalid URL"}
